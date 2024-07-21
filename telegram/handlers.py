fe9d4bf414d2f1d65c8fc31ce3bf0ef9923819c9
tzinchy <119239827+tzinchy@users.noreply.github.com>
2024-07-21 14:29:06 +0300

    requirements.txt

--- telegram/handlers.py
+++ telegram/handlers.py
@@ -18,6 +18,14 @@ async def convert_url_to_mp3(message: types.Message):
         chat_id=message.chat.id,
         action=ChatAction.UPLOAD_DOCUMENT
     )
-    file_path, title = download_audio_as_mp3(url)
-    res = FSInputFile(path = file_path, filename=title)
-    await bot.send_audio(message.chat.id, audio=res)
+    try:
+        file_path, title = download_audio_as_mp3(url)
+        res = FSInputFile(path = file_path, filename=title)
+        await bot.send_audio(message.chat.id, audio=res)
+    except Exception as e:
+        await message.reply('Произошла ошибка!')
+    finally:
+    # Удаляем файл после отправки, даже если произошла ошибка
+        if os.path.exists(file_path):
+            os.remove(file_path)
+            print(f"Файл {file_path} удален после отправки.")
